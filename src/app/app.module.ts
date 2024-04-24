893f1f4fb91104b6c518b3f96e1c1b05b62977f9
Ruben David <ruben@jobs2live.com>
2024-04-24 13:59:14 -0400

    Fernando Herrera 020 Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './listado/listado.component';
-
-
+import { HeroesModule } from './heroes/heroe.module';
 
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, HeroeComponent,ListadoComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+  ],
+  imports: [BrowserModule,HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
