a39a521ca8a92afb8ede5985e149d12ab1b557d2
Ruben David <ruben@jobs2live.com>
2024-04-25 11:32:58 -0400

    Fernando Herrera Sec-5 011 Tarea con Inputs y Modulos

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+  
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  @Input()
+  personajes: Personaje[] = []
+  
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    console.log(this.nuevo);
+    
+    this.personajes.push(this.nuevo);
+
+    console.log(this.personajes);
+    
+    
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
+    /* this.nuevo.nombre = '';
+    this.nuevo.poder = 0; */
+  }
+}
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -20,22 +20,9 @@ export class MainPageComponent {
   ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0,
-  };
-
-  agregar() {
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-
-    this.personajes.push(this.nuevo);
-
-    this.nuevo = {
-      nombre: '',
-      poder: 0,
-    };
-    /* this.nuevo.nombre = '';
-    this.nuevo.poder = 0; */
+    nombre: "Yamcha",
+    poder:1000
   }
+
+ 
 }
